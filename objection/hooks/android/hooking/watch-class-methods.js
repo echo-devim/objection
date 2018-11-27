69cd8d4a1feb6d0ba03e9951f681978a3fee1734
greg <devim@outlook.it>
2018-11-27 20:39:02 +0100

    Manage exceptions without close the application

--- objection/hooks/android/hooking/watch-class-methods.js
+++ objection/hooks/android/hooking/watch-class-methods.js
@@ -59,58 +59,67 @@ methods.map(function (method) {
             data: 'Hooking overload: ' + (i + 1) + ' for ' + method
         });
 
-        // Hook the overload.
-        target_class[method].overloads[i].implementation = function () {
+        try {
+            // Hook the overload.
+            target_class[method].overloads[i].implementation = function () {
 
-            var message = 'Called ' + target_class + '.' + method + ' (args: ' + arguments.length + ')';
+                var message = 'Called ' + target_class + '.' + method + ' (args: ' + arguments.length + ')';
 
-            // if we should include a backtrace to here, do that.
-            if (dump_backtrace) {
+                // if we should include a backtrace to here, do that.
+                if (dump_backtrace) {
 
-                message += '\nBacktrace:\n\t' + Throwable.$new().getStackTrace()
+                    message += '\nBacktrace:\n\t' + Throwable.$new().getStackTrace()
 
-                    .map(function (stack_trace_element) {
+                        .map(function (stack_trace_element) {
 
-                        return stack_trace_element.toString() + '\n\t';
-                    }).join('');
-            }
-
-            send({
-                status: 'success',
-                error_reason: NaN,
-                type: 'watch-class',
-                data: message
-            });
-
-            if (dump_args) {
-
-                // Loop the arguments and dump the values.toString()
-                for (var h = 0; h < arguments.length; h++) {
-
-                    send({
-                        status: 'success',
-                        error_reason: NaN,
-                        type: 'watch-class',
-                        data: method + ' - Arg ' + h + ': ' + (arguments[h] || '(none)').toString()
-                    });
+                            return stack_trace_element.toString() + '\n\t';
+                        }).join('');
                 }
-            }
-
-            // continue with the original method and capture the return value
-            var return_value = this[method].apply(this, arguments);
-
-            if (dump_return) {
 
                 send({
                     status: 'success',
                     error_reason: NaN,
                     type: 'watch-class',
-                    data: method + ' - Returned : ' + (return_value || '(none)').toString()
+                    data: message
                 });
-            }
 
-            // finally, return
-            return return_value;
+                if (dump_args) {
+
+                    // Loop the arguments and dump the values.toString()
+                    for (var h = 0; h < arguments.length; h++) {
+
+                        send({
+                            status: 'success',
+                            error_reason: NaN,
+                            type: 'watch-class',
+                            data: method + ' - Arg ' + h + ': ' + (arguments[h] || '(none)').toString()
+                        });
+                    }
+                }
+
+                // continue with the original method and capture the return value
+                var return_value = this[method].apply(this, arguments);
+
+                if (dump_return) {
+
+                    send({
+                        status: 'success',
+                        error_reason: NaN,
+                        type: 'watch-class',
+                        data: method + ' - Returned : ' + (return_value || '(none)').toString()
+                    });
+                }
+
+                // finally, return
+                return return_value;
+            }
+        } catch(ex) {
+            send({
+                status: 'error',
+                 error_reason: NaN,
+                 type: 'watch-class',
+                 data: ex.message
+            });
         }
     }
 });
